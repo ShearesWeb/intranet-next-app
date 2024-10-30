07c7b0a22548802889e2f69e5d0ea01f2c86314a
hrishikeshsathyian <hrishikeshsathyiancoding@gmail.com>
2024-10-30 20:08:38 +0800

    initialise supabase client instance

--- intranet-next-app/src/app/api/auth/getUserByEmail/route.ts
+++ intranet-next-app/src/app/api/auth/getUserByEmail/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'; 
 import { adminAuth } from '../../../../../lib/firebase-admin-config';
+import { supabase } from '../../../../../lib/supabase';
 
+const USER_TABLE_NAME = 'Users';
 
 export async function POST(request : Request) {
     try {
@@ -9,7 +11,12 @@ export async function POST(request : Request) {
             return NextResponse.json({ error: 'Email is required' }, { status: 400 });
           }
           const userRecord = await adminAuth.getUserByEmail(email);
-         
+          console.log('Supabase Key:', process.env.SUPABASE_KEY);    
+          const {data, error} = await supabase.from(USER_TABLE_NAME).select("*")
+          if (error) {
+            console.log("ERROR" + error);
+          }
+          console.log(data);
           return NextResponse.json({ user: userRecord.toJSON()});
     } catch (error) {
             console.error('Error fetching user data:', error);
